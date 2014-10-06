8355801bf8dbdd12f0785341da6d8c53c9a62516
Randactyl <rafowler@randactyl.com>
2014-10-06 15:05:42 -0700

    Initial attempt + bug fixes
    Single bug from not realizing the "i" was the entire path to a table
    element rather that just the index of the element.

--- InventoryRecorder.lua
+++ InventoryRecorder.lua
@@ -4,29 +4,44 @@ function IR_Register()
 	SLASH_COMMANDS["/record"] = function(arg)
 		IRData.inventory = {}
 		t = {}
-		--ZO_DeepTableCopy(PLAYER_INVENTORY.inventories[BAG_BACKPACK].slots, IRData["Inventory"])
-		--IRData["Inventory"] = PLAYER_INVENTORY.inventories[BAG_BACKPACK].slots
-		for _,v in pairs(ZO_PlayerInventoryBackpack.data) do
-			t.age = v.age
-			t.equipType = v.data.equipType
-			t.filterData = v.data.filterData
-			t.iconFile = v.data.iconFile
-			t.itemInstanceId = v.data.itemInstanceId
-			t.itemType = v.data.itemType
-			t.name = v.data.name
-			t.nameWithQuantity = v.data.nameWithQuantity
-			t.quality = v.data.quality
-			t.rawName = v.data.rawName
-			t.requiredLevel = v.data.requiredLevel
-			t.requiredVeteranRank = v.data.requiredVeteranRank
-			t.sellPrice = v.data.sellPrice
-			t.SousChef_provisioningRank = v.data.SousChef_provisioningRank
-			t.stackCount = v.data.stackCount
-			t.stackSellPrice = v.data.stackSellPrice
-			t.statValue = v.data.statValue
 
-			table.insert(IRData.inventory, t)
-			t = {}
+		for _,v in pairs(ZO_PlayerInventoryBackpack.data) do
+			local exists = false
+			local pos = nil
+
+			for i,w in pairs(IRData.inventory) do
+				if w.itemInstanceId == v.data.itemInstanceId then
+					exists = true
+					pos = i
+					break
+				end
+			end
+
+			if exists then
+				i.stackCount = i.stackCount + v.data.itemInstanceId
+				i.stackSellPrice = i.sellPrice * i.stackCount
+			else
+				t.age = v.age
+				t.equipType = v.data.equipType
+				t.filterData = v.data.filterData
+				t.iconFile = v.data.iconFile
+				t.itemInstanceId = v.data.itemInstanceId
+				t.itemType = v.data.itemType
+				t.name = v.data.name
+				t.nameWithQuantity = v.data.nameWithQuantity
+				t.quality = v.data.quality
+				t.rawName = v.data.rawName
+				t.requiredLevel = v.data.requiredLevel
+				t.requiredVeteranRank = v.data.requiredVeteranRank
+				t.sellPrice = v.data.sellPrice
+				t.SousChef_provisioningRank = v.data.SousChef_provisioningRank
+				t.stackCount = v.data.stackCount
+				t.stackSellPrice = v.data.stackSellPrice
+				t.statValue = v.data.statValue
+
+				table.insert(IRData.inventory, t)
+				t = {}
+			end
 		end
 		d("Inventory recorded to file.")
 	end
